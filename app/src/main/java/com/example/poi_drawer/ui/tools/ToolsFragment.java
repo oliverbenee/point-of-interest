537b663be7cff3f1555bfee9b62461ad79b5893b
Oliver Medoc Benée Petersen <201806928@post.au.dk>
2019-10-08 17:25:07 +0200

    Javadoc in ToolsFragment and ToolsViewModel

--- app/src/main/java/com/example/poi_drawer/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/poi_drawer/ui/tools/ToolsFragment.java
@@ -18,13 +18,21 @@ import com.example.poi_drawer.R;
 import com.example.poi_drawer.ui.map.MapFragment;
 import com.example.poi_drawer.ui.video.VideoFragment;
 
+/**
+ * The ToolsFragment contains the account changing form, and handles buttons for that view.
+ *
+ * @author Oliver Medoc Benée Petersen 201806928, Android Studio templates.
+ * @version 1.0
+ * @since 06-10-2019
+ */
+
 public class ToolsFragment extends Fragment {
 
     private Button signinbutton;
     private ToolsViewModel toolsViewModel;
 
     /**
-     * Shows the tools fragment to the user.
+     * Create and show the tools fragment to the user.
      *
      * @param inflater Handles showing the fragment.
      * @param container The view to show the fragment at.
@@ -38,6 +46,11 @@ public class ToolsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
         final TextView textView = root.findViewById(R.id.text_tools);
         toolsViewModel.getText().observe(this, new Observer<String>() {
+            /**
+             * Changes the String value to be displayed on the tools fragment. Serves as a function for debugging.
+             *
+             * @param s the string showing the error.
+             */
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
@@ -45,6 +58,11 @@ public class ToolsFragment extends Fragment {
         });
         signinbutton = root.findViewById(R.id.sign_in_button);
         signinbutton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Redirect the user to the video tutorial view, when they have signed in.
+             *
+             * @param view the current view presented. Required for method overriding, but not used.
+             */
             @Override
             public void onClick(View view) {
                 VideoFragment videoFragment = new VideoFragment();
--- app/src/main/java/com/example/poi_drawer/ui/tools/ToolsViewModel.java
+++ app/src/main/java/com/example/poi_drawer/ui/tools/ToolsViewModel.java
@@ -4,19 +4,29 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+/**
+ * The ToolsViewModel file is used as a back-up view for the ToolsFragment.
+ *
+ * @author Oliver Medoc Benée Petersen 201806928, Android Studio templates.
+ * @version 1.0
+ * @since 06-10-2019
+ */
 public class ToolsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
     /**
-     * Shows the text to tell the user, that this is the tools fragment.
+     * Constructor, that creates the title shown on the ToolsFragment.
      */
-
     public ToolsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("A view for changing account goes here. ");
     }
 
+    /**
+     * Show the text to tell the user, that the tools fragment is shown.
+     * @return String text saying "A view for changing account goes here."
+     */
     public LiveData<String> getText() {
         return mText;
     }
